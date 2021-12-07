86e3f014cae26c84c51b23f584b3c9bcc6d4da46
Yash Kumar <yashkumarjmp2002@gmail.com>
2021-12-08 01:25:15 +0530

    Upsolved

--- Challenge/1. Codeforces Round #724 (Div. 2)/C_Diluc_and_Kaeya.cpp
+++ Challenge/1. Codeforces Round #724 (Div. 2)/C_Diluc_and_Kaeya.cpp
@@ -2,7 +2,45 @@
 // Link - codeforces.com/contest/1536/problem/C
 
 
+// METHOD 1
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define pb push_back
+#define mp make_pair
+#define fora(i,a,b) for(int i=a;i<b;i++)
+#define rfora(i,a,b) for(int i=b-1;i>=a;i--)
+#define nline << "\n"
+const int mod = 1e9 + 7;
+/*-----------------------------------------------------------------------------------*/
 
+int32_t main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int t;
+	cin >> t;
+	while (t--) {
+		int n; string s;
+		cin >> n >> s;
+		map <double, int > m;
+		double d = 0, k = 0;
+		fora(i, 0, n) {
+			if (s[i] == 'D') d++;
+			else k++;
+
+			m[d / k]++;
+			cout << m[d / k] << " ";
+		} cout nline;
+	}
+
+	return 0;
+}
+
+
+
+// METHOD 2
 #include <bits/stdc++.h>
 using namespace std;
 #define int long long
