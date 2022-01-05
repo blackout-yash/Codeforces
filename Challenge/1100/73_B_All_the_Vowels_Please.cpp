8926db4eb5cb2238bc50987278d460b59ce15b69
Yash Kumar <yashkumarjmp2002@gmail.com>
2022-01-05 22:36:53 +0530

    Practice

---
+++ Challenge/1100/73_B_All_the_Vowels_Please.cpp
@@ -0,0 +1,91 @@
+// B. All the Vowels Please
+// Link - https://codeforces.com/problemset/problem/1166/B
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define int long long
+#define nline "\n"
+#define ff first
+#define ss second
+#define pb push_back
+#define pf push_front
+#define mp make_pair
+#define mm INT_MAX
+#define mn INT_MIN
+#define unsigned int32_t
+#define size(x) (x).size()
+#define all(x) (x).begin(), (x).end()
+#define MOD 1000000007
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define fora(i,a,b)  for(int i=a;i<b;i++)
+#define rfora(i,a,b) for(int i=b-1;i>=a;i--)
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x << " => "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(bool t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+/*----------------------------------------------------------------------------------------*/
+
+pair <int, int> solve(int n) {
+	fora(i, 5, n + 1) {
+		fora(j, 5, n + 1) {
+			int temp = i * j;
+			if (temp == n) return mp(i, j);
+			else if (temp > n) break;
+		}
+	} return mp(-1, -1);
+}
+
+
+int32_t main() {
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	freopen("error.txt", "w", stderr);
+#endif
+	fastio();
+
+	int n;
+	cin >> n;
+	pair<int, int> p = solve(n);
+
+	int x = p.ff, y = p.ss;
+	if (x != -1) {
+		vector <char> arr = {'a', 'e', 'i', 'o', 'u'};
+		debug(arr[0])
+		int a = 0, b = 0;
+		fora(i, 0, x) {
+			if (a == 5) a = 0;
+			b = a;
+			fora(j, 0, y) {
+				if (b == 5) b = 0;
+				cout << arr[b++];
+			} a++;
+		}
+	} else cout << "-1";
+
+
+	return 0;
+}
