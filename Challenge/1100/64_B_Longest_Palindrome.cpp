64c1d5ff306502f53df139feaf57a5258f0f88c2
Yash Kumar <yashkumarjmp2002@gmail.com>
2022-01-05 19:01:35 +0530

    Practice

---
+++ Challenge/1100/64_B_Longest_Palindrome.cpp
@@ -0,0 +1,103 @@
+// B. Longest Palindrome
+// Link - https://codeforces.com/problemset/problem/1304/B
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define int long long
+#define nline "\n"
+#define ff first
+#define ss second
+#define pb push_back
+#define pf push_front
+#define mp make_pair
+#define mm INT_MAX
+#define mn INT_MIN
+#define unsigned int32_t
+#define size(x) (x).size()
+#define all(x) (x).begin(), (x).end()
+#define MOD 1000000007
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define fora(i,a,b)  for(int i=a;i<b;i++)
+#define rfora(i,a,b) for(int i=b-1;i>=a;i--)
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x << " => "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(bool t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+/*----------------------------------------------------------------------------------------*/
+
+bool isPalidrome(string &s1, string &s2, int n) {
+	int i = 0, j = n - 1;
+	while (j >= 0) {
+		if (s1[i++] != s2[j--]) return false;
+	} return true;
+}
+
+int32_t main() {
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	freopen("error.txt", "w", stderr);
+#endif
+	fastio();
+
+	int n, m;
+	cin >> n >> m;
+	string s[n];
+	fora(i, 0, n) cin >> s[i];
+
+	string front, middle, back;
+	fora(i, 0, n) {
+		if (s[i] != "-1") {
+			fora(j, 0, n) {
+				if (i != j && s[j] != "-1") {
+					if (isPalidrome(s[i], s[j], m)) {
+						front += s[i];
+						back = s[j] + back;
+						s[i] = s[j] = "-1";
+					}
+				}
+			}
+		}
+	}
+
+	fora(i, 0, n) {
+		if (s[i] != "-1") {
+			bool flag = true;
+			int l = 0, r = m - 1;
+			while (l <= r) {
+				if (s[i][l++] != s[i][r--]) {
+					flag = false; break;
+				}
+			}
+
+			if (flag) {
+				middle = s[i]; break;
+			}
+		}
+	}
+	cout << size(front) + size(middle) + size(back) << nline << front + middle + back;
+	return 0;
+}
