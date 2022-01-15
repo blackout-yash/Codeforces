388f444c11adecd58adf5a5fcb695f8a0bc62ff1
Yash Kumar <yashkumarjmp2002@gmail.com>
2022-01-15 15:15:35 +0530

    Practice

---
+++ Challenge/1200/65_C_Boats_Competition.cpp
@@ -0,0 +1,93 @@
+// C. Boats Competition
+// Link - https://codeforces.com/problemset/problem/1399/C
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define int long long
+#define nline "\n"
+#define ff first
+#define ss second
+#define pb push_back
+#define pf push_front
+#define mp make_pair
+#define mm (9*1e18)
+#define mn ((-9)*1e18)
+#define unsigned int32_t
+#define size(x) (x).size()
+#define all(x) (x).begin(), (x).end()
+#define MOD 1000000007
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define fora(i,a,b)  for(int i=a;i<b;i++)
+#define rfora(i,a,b) for(int i=b-1;i>=a;i--)
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x << " => "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(bool t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+/*----------------------------------------------------------------------------------------*/
+
+
+int32_t main() {
+#ifndef ONLINE_JUDGE
+	freopen("error.txt", "w", stderr);
+#endif
+	fastio();
+	/*-------👌Never Give Up👌-------*/
+
+
+	int tt;
+	cin >> tt;
+	while (tt--) {
+		int n, x, maximum = mn;
+		cin >> n;
+		map <int, int> m, map;
+		fora(i, 0, n) {
+			cin >> x;
+			maximum = max(maximum, x);
+			m[x]++;
+		}
+
+		if (n == 1) cout << "0\n";
+		else {
+			int temp, ans = 1;
+			fora(i, 2, (2 * maximum) + 1) {
+				map = m; temp = 0;
+				for (auto x : map) {
+					int value1 = x.ff, key1 = map[value1], value2 = i - value1, key2 = map[value2];
+
+					if (value1 == value2) temp += (key1 / 2);
+					else {
+						int minimum = min(key1, key2);
+						temp += minimum;
+						map[value1] -= minimum;
+						map[value2] -= minimum;
+					}
+				} ans = max(ans, temp);
+			} cout << ans << nline;
+		}
+	}
+
+	return 0;
+}
