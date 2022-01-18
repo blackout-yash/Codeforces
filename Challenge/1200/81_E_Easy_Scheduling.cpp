b478e04d553dc959b2570c7a747edbc069f1c3d8
Yash Kumar <yashkumarjmp2002@gmail.com>
2022-01-18 09:31:03 +0530

    Practice

---
+++ Challenge/1200/81_E_Easy_Scheduling.cpp
@@ -0,0 +1,82 @@
+// E. Easy Scheduling
+// Link - https://codeforces.com/contest/1578/problem/E
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define int long long
+#define nline "\n"
+#define ff first
+#define ss second
+#define pb push_back
+#define pf push_front
+#define mp make_pair
+#define mm (9*1e18)
+#define mn ((-9)*1e18)
+#define unsigned int32_t
+#define size(x) (x).size()
+#define all(x) (x).begin(), (x).end()
+#define MOD 1000000007
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define fora(i,a,b)  for(int i=a;i<b;i++)
+#define rfora(i,a,b) for(int i=b-1;i>=a;i--)
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x << " => "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(bool t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+/*----------------------------------------------------------------------------------------*/
+
+int _power(int a, int b) {
+	if (b == 0) return 1;
+	long long res = _power(a, b / 2);
+	if (b % 2) return res * res * a;
+	return res * res;
+}
+
+
+int32_t main() {
+#ifndef ONLINE_JUDGE
+	freopen("error.txt", "w", stderr);
+#endif
+	fastio();
+	/*-------👌Never Give Up👌-------*/
+
+
+	int tt;
+	cin >> tt;
+	while (tt--) {
+		int n, k;
+		cin >> n >> k;
+
+		int temp = ceil(log2(k)), diff = (_power(2, temp) - 1);
+		int sum = (_power(2, n) - 1) - diff, ans = sum / k;
+		if (sum % k) ans++;
+		debug(sum)
+		if (sum >= 0) cout << ans + temp << nline;
+		else cout << n << nline;
+	}
+
+	return 0;
+}
