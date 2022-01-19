58f04ca5780b215ce17d01900625f3805a897d28
Yash Kumar <yashkumarjmp2002@gmail.com>
2022-01-19 18:01:18 +0530

    Practice

---
+++ Challenge/1300/02_C_Coin_Rows.cpp
@@ -0,0 +1,81 @@
+// C. Coin Rows
+// Link - https://codeforces.com/problemset/problem/1555/C
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define int long long
+#define nline "\n"
+#define ff first
+#define ss second
+#define pb push_back
+#define pf push_front
+#define mp make_pair
+#define mm (9*1e18)
+#define mn ((-9)*1e18)
+#define unsigned int32_t
+#define size(x) (x).size()
+#define all(x) (x).begin(), (x).end()
+#define MOD 1000000007
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define fora(i,a,b)  for(int i=a;i<b;i++)
+#define rfora(i,a,b) for(int i=b-1;i>=a;i--)
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x << " => "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(bool t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+/*----------------------------------------------------------------------------------------*/
+
+int32_t main() {
+#ifndef ONLINE_JUDGE
+	freopen("error.txt", "w", stderr);
+#endif
+	fastio();
+	/*-------👌Never Give Up👌-------*/
+
+
+	int tt;
+	cin >> tt;
+	while (tt--) {
+		int m;
+		cin >> m;
+		vector <vector <int>> arr(2, vector <int> (m));
+		int sum1 = 0, sum2 = 0, ans = mm;
+		fora(i, 0, 2) {
+			fora(j, 0, m) {
+				cin >> arr[i][j];
+				if (!i) sum1 += arr[i][j];
+			}
+		}
+
+		fora(i, 0, m) {
+			sum1 -= arr[0][i];
+			if (i) sum2 += arr[1][i - 1];
+			ans = min(ans, max(sum1, sum2));
+		} cout << ans << nline;
+	}
+
+	return 0;
+}
