02479e0f2669217b6570e54f20a4a4139ea2656f
Yash Kumar <yashkumarjmp2002@gmail.com>
2022-01-25 10:23:32 +0530

    Practice

---
+++ Challenge/1300/36_B_Letters_Shop.cpp
@@ -0,0 +1,80 @@
+// B. Letters Shop
+// Link - https://codeforces.com/problemset/problem/1187/B
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define int long long
+#define nline "\n"
+#define ff first
+#define ss second
+#define pb push_back
+#define pf push_front
+#define mp make_pair
+#define mm (9*1e18)
+#define mn ((-9)*1e18)
+#define unsigned int32_t
+#define size(x) (x).size()
+#define all(x) (x).begin(), (x).end()
+#define MOD 1000000007
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define fora(i,a,b)  for(int i=a;i<b;i++)
+#define rfora(i,a,b) for(int i=b-1;i>=a;i--)
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x << " => "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(bool t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+/*----------------------------------------------------------------------------------------*/
+
+
+int32_t main() {
+#ifndef ONLINE_JUDGE
+	freopen("error.txt", "w", stderr);
+#endif
+	fastio();
+	/*-------👌Never Give Up👌-------*/
+
+
+	int n, m, ans; string s;
+	cin >> n >> s;
+	vector <vector <int>> arr(26);
+	fora(i, 0, n) {
+		int temp = unsigned(s[i] - 'a');
+		arr[temp].pb(i + 1);
+	}
+
+	cin >> m;
+	fora(i, 0, m) {
+		cin >> s; ans = 0;
+		map <char, int> m;
+		fora(j, 0, size(s)) {
+			m[s[j]]++;
+			int temp = unsigned(s[j] - 'a'), key = m[s[j]] - 1;
+			ans = max(ans, arr[temp][key]);
+		} cout << ans << nline;
+	}
+
+	return 0;
+}
---
+++ Challenge/1300/37_A_Dressem_in_Vests.cpp
@@ -0,0 +1,82 @@
+// A. Dress'em in Vests!
+// Link - https://codeforces.com/problemset/problem/161/A
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define int long long
+#define nline "\n"
+#define ff first
+#define ss second
+#define pb push_back
+#define pf push_front
+#define mp make_pair
+#define mm (9*1e18)
+#define mn ((-9)*1e18)
+#define unsigned int32_t
+#define size(x) (x).size()
+#define all(x) (x).begin(), (x).end()
+#define MOD 1000000007
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define fora(i,a,b)  for(int i=a;i<b;i++)
+#define rfora(i,a,b) for(int i=b-1;i>=a;i--)
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x << " => "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(bool t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+/*----------------------------------------------------------------------------------------*/
+
+
+int32_t main() {
+#ifndef ONLINE_JUDGE
+	freopen("error.txt", "w", stderr);
+#endif
+	fastio();
+	/*-------👌Never Give Up👌-------*/
+
+
+	int n, m, x, y;
+	cin >> n >> m >> x >> y;
+	vector <int> a(n), b(m);
+	fora(i, 0, n) cin >> a[i];
+	fora(i, 0, m) cin >> b[i];
+
+	vector <pair <int, int>> arr;
+	int range = 0;
+	fora(i, 0, n) {
+		int temp = a[i] - x, index = lower_bound(b.begin() + range, b.end(), temp) - b.begin();
+
+		if (index != m && b[index] <= a[i] + y) {
+			range = index + 1;
+			arr.pb(mp(i + 1, index + 1));
+		} if (range == m) break;
+	}
+
+	cout << size(arr) << nline;
+	fora(i, 0, size(arr)) cout << arr[i].ff << " " << arr[i].ss << nline;
+
+
+	return 0;
+}
