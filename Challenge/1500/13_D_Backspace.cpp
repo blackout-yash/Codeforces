f41058df7910e7670e7a6f8edb00212ea2e98e5e
Yash Kumar <yashkumarjmp2002@gmail.com>
2022-02-20 00:30:54 +0530

    Practice

---
+++ Challenge/1500/13_D_Backspace.cpp
@@ -0,0 +1,85 @@
+// D. Backspace
+// Link - https://codeforces.com/problemset/problem/1553/D
+
+
+
+/*--------------------------------------👻 Yash 👻--------------------------------------*/
+// #pragma GCC optimize("Ofast")
+#include <bits/stdc++.h>
+using namespace std;
+#define ff first
+#define ss second
+#define nline "\n"
+#define mm (9*1e18)
+#define mp make_pair
+#define pb push_back
+#define pf push_front
+#define int long long
+#define mn ((-9)*1e18)
+#define MOD 1000000007
+using namespace chrono;
+#define unsigned int32_t
+#define size(x) (x).size()
+#define PI 3.141592653589793238462
+#define all(x) (x).begin(), (x).end()
+#define set_bits __builtin_popcountll
+#define fora(i,a,b)  for(int i=a;i<b;i++)
+#define rfora(i,a,b) for(int i=b-1;i>=a;i--)
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x << " => "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(bool t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+/*----------------------------------------------------------------------------------------*/
+
+void solve() {
+	/*-------👌Never Give Up👌-------*/
+
+	int tt;
+	cin >> tt;
+	while (tt--) {
+		string s1, s2;
+		cin >> s1 >> s2;
+		int j = size(s2) - 1;
+		rfora(i, 0, size(s1)) {
+			if (s1[i] == s2[j]) j--;
+			else i--;
+			if (j == -1) break;
+		} (j == -1) ? cout << "YES\n" : cout << "NO\n";
+	}
+}
+
+
+int32_t main() {
+#ifndef ONLINE_JUDGE
+	freopen("error.txt", "w", stderr);
+#endif
+	fastio();
+	auto start1 = high_resolution_clock::now();
+	solve();
+	auto stop1 = high_resolution_clock::now();
+	auto duration = duration_cast<microseconds>(stop1 - start1);
+#ifndef ONLINE_JUDGE
+	cerr << "Time: " << duration . count() / 1000 << "ms" << endl;
+#endif
+	return 0;
+}
---
+++ Challenge/1500/17_C_Vasya_and_Multisets.cpp
@@ -0,0 +1,123 @@
+// C. Vasya and Multisets
+// Link - https://codeforces.com/contest/1051/problem/C
+
+
+
+/*--------------------------------------👻 Yash 👻--------------------------------------*/
+
+// #include<ext/pb_ds/assoc_container.hpp>
+// #include<ext/pb_ds/tree_policy.hpp>
+// #pragma GCC optimize("Ofast")
+// using namespace __gnu_pbds;
+#include <bits/stdc++.h>
+using namespace std;
+#define ff first
+#define ss second
+#define nline "\n"
+#define mm (9*1e18)
+#define mp make_pair
+#define pb push_back
+#define pf push_front
+#define int long long
+#define mn ((-9)*1e18)
+#define MOD 1000000007
+using namespace chrono;
+#define unsigned int32_t
+#define size(x) (x).size()
+#define PI 3.141592653589793238462
+#define all(x) (x).begin(), (x).end()
+#define set_bits __builtin_popcountll
+#define fora(i,a,b)  for(int i=a;i<b;i++)
+#define rfora(i,a,b) for(int i=b-1;i>=a;i--)
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);
+// typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds; // find_by_order, order_of_key
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x << " => "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(bool t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+// void _print(pbds v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+/*----------------------------------------------------------------------------------------*/
+
+
+void solve() {
+	/*-------👌Never Give Up👌-------*/
+
+	int n;
+	cin >> n;
+	map <int, int> m;
+	vector <int> arr(n);
+	fora(i, 0, n) {
+		cin >> arr[i];
+		m[arr[i]]++;
+	}
+	vector <pair<int, int>> store;
+	for (auto x : m) store.pb({x.ss, x.ff});
+	sort(all(store));
+
+	vector <int> ans1, ans2;
+	bool flag = true;
+	fora(i, 0, size(store)) {
+		if (flag) {
+			if (store[i].ff == 1) {
+				ans1.pb(store[i].ss);
+				flag = false;
+			}
+		} else {
+			if (store[i].ff != 2) {
+				ans2.pb(store[i].ss);
+				flag = true;
+			}
+		}
+	} sort(all(ans1)); sort(all(ans2));
+
+	if (flag) {
+		cout << "YES\n";
+		map <int, int> store;
+		fora(i, 0, n) {
+			bool flag1 = binary_search(all(ans1), arr[i]), flag2 = binary_search(all(ans2), arr[i]);
+			if (flag1) cout << "A";
+			else if (flag2) {
+				if (store[arr[i]]) cout << "A";
+				else {
+					store[arr[i]]++;
+					cout << "B";
+				}
+			} else cout << "B";
+		}
+	} else cout << "NO";
+}
+
+
+int32_t main() {
+#ifndef ONLINE_JUDGE
+	freopen("error.txt", "w", stderr);
+#endif
+	fastio();
+	auto start1 = high_resolution_clock::now();
+	solve();
+	auto stop1 = high_resolution_clock::now();
+	auto duration = duration_cast<microseconds>(stop1 - start1);
+#ifndef ONLINE_JUDGE
+	cerr << "Time: " << duration . count() / 1000 << "ms" << endl;
+#endif
+	return 0;
+}
