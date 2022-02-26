cb2230a817644ff586269ac07dcc2eadde431c67
Yash Kumar <yashkumarjmp2002@gmail.com>
2022-02-26 17:20:03 +0530

    Practice

---
+++ Challenge/1500/32_C_Cyclic_Permutations.cpp
@@ -0,0 +1,110 @@
+// C. Cyclic Permutations
+// Link - https://codeforces.com/problemset/problem/1391/C
+
+
+
+/*--------------------------------------👻 Yash 👻--------------------------------------*/
+/*----------------------------------😔 Semi - Editorial Op! 😔---------------------------------*/
+
+// #include<ext/pb_ds/assoc_container.hpp>
+// #include<ext/pb_ds/tree_policy.hpp>
+// #pragma GCC optimize("Ofast")
+// using namespace __gnu_pbds;
+#include <bits/stdc++.h>
+using namespace std;
+#define ff first
+#define ss second
+#define nline "\n"
+#define mm (9*1e18)
+#define mp make_pair
+#define pb push_back
+#define pf push_front
+#define int long long
+#define mn ((-9)*1e18)
+#define MOD 1000000007
+using namespace chrono;
+#define unsigned int32_t
+#define size(x) (x).size()
+#define PI 3.141592653589793238462
+#define all(x) (x).begin(), (x).end()
+#define set_bits __builtin_popcountll
+#define fora(i,a,b)  for(int i=a;i<b;i++)
+#define rfora(i,a,b) for(int i=b-1;i>=a;i--)
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);
+// typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds; // find_by_order, order_of_key
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x << " => "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(bool t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+// void _print(pbds v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+/*----------------------------------------------------------------------------------------*/
+
+int powMod(int a, int b, int m = MOD) {
+	a %= m;
+	int res = 1;
+	while (b > 0) {
+		if (b & 1)
+			res = res * a % m;
+		a = a * a % m;
+		b >>= 1;
+	}
+	return res;
+}
+
+int fact(int n) {
+	int ans = 1;
+	fora(i, 1, n + 1) ans = ((ans % MOD) * (i % MOD)) % MOD;
+	return ans;
+}
+
+void solve() {
+	/*-------👌Never Give Up👌-------*/
+
+	int n;
+	cin >> n;
+	int ans = fact(n) - powMod(2, n - 1);
+	if (ans >= 0) cout << ans;
+	else cout << ans + MOD;
+}
+
+// Unimodal Permutation
+// Unimodal Permutation = C(n - 1, 0) + C(n - 1, 1) +...+ C(n - 1, n - 1)
+// C(n - 1, 0) + C(n - 1, 1) +...+ C(n - 1, n - 1) = 2 ^ (n - 1)
+// Proof: https://stats.stackexchange.com/questions/27266/simplify-sum-of-combinations-with-same-n-all-possible-values-of-k
+
+
+int32_t main() {
+#ifndef ONLINE_JUDGE
+	freopen("error.txt", "w", stderr);
+#endif
+	fastio();
+	auto start1 = high_resolution_clock::now();
+	int tt = 1;
+	// cin >> tt;
+	while (tt--) {solve();}
+	auto stop1 = high_resolution_clock::now();
+	auto duration = duration_cast<microseconds>(stop1 - start1);
+#ifndef ONLINE_JUDGE
+	cerr << "Time: " << duration . count() / 1000 << "ms" << endl;
+#endif
+	return 0;
+}
---
+++ Challenge/1500/40_C_Edgy_Trees.cpp
@@ -0,0 +1,133 @@
+// C. Edgy Trees
+// Link - https://codeforces.com/contest/1139/problem/C
+
+
+
+/*--------------------------------------👻 Yash 👻--------------------------------------*/
+/*----------------------------------😔 Editorial Op! 😔---------------------------------*/
+
+// #include<ext/pb_ds/assoc_container.hpp>
+// #include<ext/pb_ds/tree_policy.hpp>
+// #pragma GCC optimize("Ofast")
+// using namespace __gnu_pbds;
+#include <bits/stdc++.h>
+using namespace std;
+#define ff first
+#define ss second
+#define nline "\n"
+#define mm (9*1e18)
+#define mp make_pair
+#define pb push_back
+#define pf push_front
+#define int long long
+#define mn ((-9)*1e18)
+#define MOD 1000000007
+using namespace chrono;
+#define unsigned int32_t
+#define size(x) (x).size()
+#define PI 3.141592653589793238462
+#define all(x) (x).begin(), (x).end()
+#define set_bits __builtin_popcountll
+#define fora(i,a,b)  for(int i=a;i<b;i++)
+#define rfora(i,a,b) for(int i=b-1;i>=a;i--)
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);
+int modAdd(int a, int b, int m = MOD) {a = a % m; b = b % m; return (((a + b) % m) + m) % m;}
+int modSub(int a, int b, int m = MOD) {a = a % m; b = b % m; return (((a - b) % m) + m) % m;}
+int modMul(int a, int b, int m = MOD) {a = a % m; b = b % m; return (((a * b) % m) + m) % m;}
+int solution(int ans, int m = MOD) {return (ans < 0) ? ans + m : ans;}
+// typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds; // find_by_order, order_of_key
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x << " => "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(bool t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+// void _print(pbds v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+/*----------------------------------------------------------------------------------------*/
+const int n = 1e5 + 10;
+vector <int> graph[n];
+vector <int> path, vis(n);
+
+void dfs(int root) {
+	vis[root] = 1;
+	path.pb(root);
+
+	for (int child : graph[root]) {
+		if (vis[child]) continue;
+		dfs(child);
+	}
+}
+
+int powMod(int a, int b, int m = MOD) {
+	a %= m;
+	int res = 1;
+	while (b > 0) {
+		if (b & 1)
+			res = res * a % m;
+		a = a * a % m;
+		b >>= 1;
+	}
+	return res;
+}
+
+void solve() {
+	/*-------👌Never Give Up👌-------*/
+
+	int n, k;
+	cin >> n >> k;
+	fora(i, 1, n) {
+		int v1, v2, c;
+		cin >> v1 >> v2 >> c;
+
+		if (!c) {
+			graph[v1].pb(v2);
+			graph[v2].pb(v1);
+		}
+	}
+
+	int ans = powMod(n, k);
+	fora(i, 1, n + 1) {
+		if (vis[i]) {}
+		else {
+			dfs(i);
+			int size = size(path);
+			ans = modSub(ans, powMod(size, k));
+			path.clear();
+		}
+	} cout << solution(ans);
+}
+
+
+int32_t main() {
+#ifndef ONLINE_JUDGE
+	freopen("error.txt", "w", stderr);
+#endif
+	fastio();
+	auto start1 = high_resolution_clock::now();
+	int tt = 1;
+	// cin >> tt;
+	while (tt--) {solve();}
+	auto stop1 = high_resolution_clock::now();
+	auto duration = duration_cast<microseconds>(stop1 - start1);
+#ifndef ONLINE_JUDGE
+	cerr << "Time: " << duration . count() / 1000 << "ms" << endl;
+#endif
+	return 0;
+}
