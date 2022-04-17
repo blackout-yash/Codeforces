7c4d4d274d96921e860ec61b2cd6464f974e0bde
Yash Kumar <yashkumarjmp2002@gmail.com>
2022-04-18 01:43:24 +0530

    Practice

---
+++ Challenge/1600/65_K_The_Robot.cpp
@@ -0,0 +1,119 @@
+// K. The Robot
+// Link - https://codeforces.com/problemset/problem/1468/K
+
+
+
+/*--------------------------------------👻 Yash 👻--------------------------------------*/
+
+#ifdef ONLINE_JUDGE
+#pragma GCC optimize("Ofast")
+#endif
+
+// #include<ext/pb_ds/assoc_container.hpp>
+// #include<ext/pb_ds/tree_policy.hpp>
+// using namespace __gnu_pbds;
+#include <bits/stdc++.h>
+using namespace std;
+#define ff first
+#define ss second
+#define nline "\n"
+#define mm (9*1e18)
+#define mp make_pair
+#define pb push_back
+#define pf push_front
+#define int long long
+#define mn ((-9)*1e18)
+#define MOD 1000000007
+using namespace chrono;
+#define unsigned int32_t
+#define size(x) (x).size()
+#define double long double
+#define PI 3.141592653589793238462
+#define all(x) (x).begin(), (x).end()
+#define set_bits __builtin_popcountll
+#define fora(i,a,b)  for(int i=a;i<b;i++)
+#define rfora(i,a,b) for(int i=b-1;i>=a;i--)
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);
+// typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds; // find_by_order, order_of_key
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x << " => "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(bool t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+// void _print(pbds v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+/*----------------------------------------------------------------------------------------*/
+
+
+void solve() {
+	/*-------__👌Never Give Up👌__-------*/
+
+	string s;
+	cin >> s;
+
+	vector <pair<int, int>> arr;
+	int x = 0, y = 0;
+	for (auto z : s) {
+		if (z == 'L') x--;
+		else if (z == 'R') x++;
+		else if (z == 'U') y++;
+		else y--;
+
+		arr.pb({x, y});
+	}
+
+	int xans = 0, yans = 0;
+	for (auto x : arr) {
+		int xtemp = 0, ytemp = 0;
+		for (auto y : s) {
+			int temp1 = xtemp, temp2 = ytemp;
+			if (y == 'L') temp1--;
+			else if (y == 'R') temp1++;
+			else if (y == 'U') temp2++;
+			else temp2--;
+
+			if (temp1 == x.ff && temp2 == x.ss) {}
+			else xtemp = temp1, ytemp = temp2;
+		}
+
+		if (xtemp == ytemp && !xtemp) {
+			xans = x.ff; yans = x.ss;
+			break;
+		}
+	} cout << xans << " " << yans << nline;
+}
+
+
+int32_t main() {
+#ifndef ONLINE_JUDGE
+	freopen("error.txt", "w", stderr);
+#endif
+	fastio();
+	auto start1 = high_resolution_clock::now();
+	int tt = 1;
+	cin >> tt;
+	while (tt--) {solve();}
+	auto stop1 = high_resolution_clock::now();
+	auto duration = duration_cast<microseconds>(stop1 - start1);
+#ifndef ONLINE_JUDGE
+	cerr << "Time: " << duration . count() / 1000 << "ms" << endl;
+#endif
+	return 0;
+}
---
+++ Challenge/1600/66_C_Row_GCD.cpp
@@ -0,0 +1,115 @@
+// C. Row GCD
+// Link - https://codeforces.com/contest/1459/problem/C
+
+
+
+/*--------------------------------------👻 Yash 👻--------------------------------------*/
+/*----------------------------------😔 Editorial Op! 😔---------------------------------*/
+
+#ifdef ONLINE_JUDGE
+#pragma GCC optimize("Ofast")
+#endif
+
+// #include<ext/pb_ds/assoc_container.hpp>
+// #include<ext/pb_ds/tree_policy.hpp>
+// using namespace __gnu_pbds;
+#include <bits/stdc++.h>
+using namespace std;
+#define ff first
+#define ss second
+#define nline "\n"
+#define mm (9*1e18)
+#define mp make_pair
+#define pb push_back
+#define pf push_front
+#define int long long
+#define mn ((-9)*1e18)
+#define MOD 1000000007
+using namespace chrono;
+#define unsigned int32_t
+#define size(x) (x).size()
+#define double long double
+#define PI 3.141592653589793238462
+#define all(x) (x).begin(), (x).end()
+#define set_bits __builtin_popcountll
+#define fora(i,a,b)  for(int i=a;i<b;i++)
+#define rfora(i,a,b) for(int i=b-1;i>=a;i--)
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);
+// typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds; // find_by_order, order_of_key
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x << " => "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(bool t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+// void _print(pbds v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+/*----------------------------------------------------------------------------------------*/
+
+// gcd(a, b, c) = gcd(a, gcd(b, c))
+
+// gcd(a, b) = gcd(a, |a - b|)
+
+// gcd(a + k, b + k, c + k)
+// 			= gcd(a + k, gcd(b + k, c + k))
+// 			= gcd(a + k, gcd(b + k, |(b + k) - (c + k)|))
+// 			= gcd(a + k, gcd(b + k, b - c ))
+// 			= gcd(a + k, b + k, b - c)
+// 			= gcd(a + k, a - b, b - c)
+
+
+void solve() {
+	/*-------__👌Never Give Up👌__-------*/
+
+	int n, m;
+	cin >> n >> m;
+
+	vector <int> arr(n);
+	for (auto &xx : arr) cin >> xx;
+
+	int gcd = 0;
+	fora(i, 0, n - 1) {
+		if (i) gcd = __gcd(gcd, abs(arr[i] - arr[i + 1]));
+		else gcd = abs(arr[0] - arr[1]);
+	}
+
+	fora(i, 0, m) {
+		int x; cin >> x;
+		cout << __gcd(arr[0] + x, gcd) << " ";
+	}
+
+}
+
+
+int32_t main() {
+#ifndef ONLINE_JUDGE
+	freopen("error.txt", "w", stderr);
+#endif
+	fastio();
+	auto start1 = high_resolution_clock::now();
+	int tt = 1;
+	// cin >> tt;
+	while (tt--) {solve();}
+	auto stop1 = high_resolution_clock::now();
+	auto duration = duration_cast<microseconds>(stop1 - start1);
+#ifndef ONLINE_JUDGE
+	cerr << "Time: " << duration . count() / 1000 << "ms" << endl;
+#endif
+	return 0;
+}
